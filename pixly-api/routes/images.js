53174bd9c846a2ca371f1b8fcc741af286262048
Chase Westover <chasejwestover@gmail.com>
2021-04-22 15:31:19 -0500

    backend

--- pixly-api/routes/images.js
+++ pixly-api/routes/images.js
@@ -1,13 +1,14 @@
 const db = require('../db');
 const express = require('express');
-const {upload} = require("../services/ImageUpload");
+const { upload } = require("../services/ImageUpload");
 const { ensureLoggedIn } = require('../middlewear/auth');
 const fileUpload = require('express-fileupload');
-const exifr = require('exifr');
+// const exifr = require('exifr');
 
 
 const router = express.Router();
 
+//upload new image
 router.post('/', ensureLoggedIn, fileUpload(), upload, async (req, res, next) => {
     // console.log(await exifr.parse(req.files.image.data, true))
     const url = res.locals.data.Location;
@@ -18,6 +19,31 @@ router.post('/', ensureLoggedIn, fileUpload(), upload, async (req, res, next) =>
         RETURNING location, camera, username, is_private, url
     `, [null, null, username, false, url]);
 
+    const image = results.rows[0];
+    return res.json({ image });
+});
+
+//get all public images
+router.get('/', async (req, res, next) => {
+    const results = await db.query(`
+        SELECT * FROM images
+        WHERE is_private = false
+    `);
+    const images = results.rows;
+    return res.json({ images });
+});
+
+//get public image by id
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    const results = await db.query(`
+        SELECT * FROM images
+        WHERE is_private = false and
+        id = $1
+    `, [id]);
+    if (results.rows.length === 0) {
+        return res.json({ error: "private image or image does not exist" });
+    }
     const image = results.rows[0];
     return res.json({ image });
 });
--- pixly-api/routes/users.js
+++ pixly-api/routes/users.js
@@ -1,6 +1,6 @@
 const db = require('../db');
 const express = require('express');
-const { authenticateJWT } = require('../middlewear/auth');
+const { authenticateJWT, ensureCorrectUser } = require('../middlewear/auth');
 
 const router = express.Router()
 
@@ -12,5 +12,35 @@ router.get('/', async (req, res, next) => {
   const users = results.rows;
   return res.json({users});
 })
+
+
+//user get private image by id
+router.get('/:username/images/:id', ensureCorrectUser, async (req, res, next) => {
+  const {id, username} = req.params
+  const results = await db.query(`
+        SELECT * FROM images
+        WHERE id = $1 and username = $2
+    `, [id, username]);
+  if (results.rows.length === 0) {
+    return res.json({ error: "not your image or image does not exist" });
+  }
+  const image = results.rows[0];
+  return res.json({ image });
+});
+
+//user get all images
+router.get('/:username/images', ensureCorrectUser, async (req, res, next) => {
+  const {username} = req.params
+  const results = await db.query(`
+        SELECT * FROM images
+        WHERE username = $1
+    `, [username]);
+  if (results.rows.length === 0) {
+    return res.json({ error: "upload some images, doofus!" });
+  }
+  const images = results.rows;
+  return res.json({ images });
+});
+
 
 module.exports = router;
