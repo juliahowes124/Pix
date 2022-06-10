7f6ef30c887b19a4d55674076594302485e413ae
Julia Howes <julia.howes@gmail.com>
2022-06-09 21:46:17 -0230

    color scheme

--- pixly-spa/src/components/PageLayout.js
+++ pixly-spa/src/components/PageLayout.js
@@ -2,7 +2,7 @@ import {Box, Text} from '@chakra-ui/react'
 
 export function PageLayout({title, children}) {
   return (
-    <Box p="4rem">
+    <Box px="4rem">
       <Text fontSize="2rem" textAlign="center" mb="4rem">{title}</Text>
       {children}
     </Box>
